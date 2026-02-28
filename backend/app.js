dcb0106c00c5dc522637081ce234b9e0d04c9887
milon <milonmondolmd33@gmail.com>
2026-03-01 00:49:24 +0600

    update all

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://protfolio-frontend-2j8c.onrender.com",
     credentials: true,
   })
 );
